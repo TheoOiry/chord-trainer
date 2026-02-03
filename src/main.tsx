13d7da6cbd065e6f8de4b8288edc3d6627fc77b3
TheoOiry <theo.oiry@yahoo.fr>
2026-02-03 23:28:41 +0100

    feat: add vercel statistics

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { ChordTypeProvider } from "./providers/ChordTypeProvider";
 import { MidiProvider } from "./providers/MidiProvider";
 import { ChordSequenceProvider } from "./providers/ChordSequenceProvider.tsx";
 import { SamplerProvider } from "./providers/SamplerProvider.tsx";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -18,5 +20,7 @@ createRoot(document.getElementById("root")!).render(
         </MidiProvider>
       </SamplerProvider>
     </ChordTypeProvider>
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 );
